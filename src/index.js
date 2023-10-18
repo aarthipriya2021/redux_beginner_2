08f44b3981dc6d1af689b3bbb51e097560869545
Aarthipriya A <a.aarthipriya2021@gmail.com>
2023-10-18 18:53:26 +0530

    posts application using redux

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Store } from "./app/store";
+import { Provider } from "react-redux";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={Store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
